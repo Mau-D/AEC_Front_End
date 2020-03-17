fd339ff9456d6937f21c894e5e623a12c50d3712
Mau-D <harveymaud@gmail.com>
2020-03-17 11:25:35 -0400

    Correction Flash

--- Programmation1/Flash Version 2020/Flash Version 2020/js/flash.js
+++ Programmation1/Flash Version 2020/Flash Version 2020/js/flash.js
@@ -1,16 +1,36 @@
 //Exercice sur les évènements de la souris
 let positionflash = 100;
-
+let largeurEcran =  screen.width - 200; //connaître la largeur de l'écran -200(position de départ + largeur de l'image)
+console.log(largeurEcran);
 //Déplacer vers la gauche
-$( "#boutonGauche" ).click(function() {
-    positionflash += 100;
-    $('#flashID').css("left" , positionflash);
+$("#boutonGauche" ).click(function() {
+    if(positionflash > 100) {
+        positionflash -= 100;
+        $('#flashID').css("left", positionflash);
+    }
 });
+//Correction
+//$("#boutonGauche").click(function() {
+//     gauche -=10;
+//
+//     //$('#imgFlash').css("left",gauche+"px");
+//     $( "#imgFlash" ).animate({ "left": "-=50px" }, "slow" );
+// });
 //Déplacer vers la droite
 $( "#boutonDroite" ).click(function() {
-    positionflash -= 100;
-    $('#flashID').css("left" , positionflash);
+    if(positionflash < largeurEcran ) {
+        positionflash += 100;
+        $('#flashID').css("left", positionflash);
+        console.log(positionflash);
+    }
 });
+//Correction
+//$("#boutonDroite").click(function() {
+//     gauche += 10;
+//     //$('#imgFlash').css("left",gauche+"px");}
+//     $("#imgFlash").animate({"left": "+=50px"}, "slow");
+// });
+
 //Changer l'image
 let image = "flash1";
 $( "#boutonChangerImage" ).click(function() {
@@ -23,6 +43,16 @@ $( "#boutonChangerImage" ).click(function() {
         image = "flash1";
     }
 });
+//Correction
+//$("#boutonChangerImage").click(function() {
+//     var imageFlashSRC = $('#flashID').attr("src");
+//     if(imageFlashSRC === "flash.jpg"){
+//         $('#flashID').attr("src","flash2.jpg");
+//     }
+//     else{
+//         $('#flashID').attr("src","flash.jpg");
+//     }
+// });
 //Enlever ou faire disparaître l'image
 $( "#boutonEnleverImage" ).click(function() {
     $('#flashID').hide("slow");
@@ -36,35 +66,69 @@ $( "#boutonAjouterAttribut" ).click(function() {
     $('#flashID').attr("alt", "photo de Flash");
 });
 //Ajouter un titre h1 avant la photo
-$( "#boutonAjouterTitre" ).click(function() {
-    $( "#flashID" ).before( "<h1>Justice League</h1>");
-    $( "h1" ).addClass( "text-white" );
-});
+let titre =false;
+    $("#boutonAjouterTitre").click(function () {
+        if (!titre) {
+            $("#flashID").before("<h1>Justice League</h1>");
+            $("h1").addClass("text-white");
+            titre = true; //Pour afficher le titre une seule fois
+        }
+    });
+
 //Dupliquer
+let compteurClone = 0;
 $( "#boutonDupliquer" ).click(function() {
     $( "#flashID" ).clone().appendTo( "#clonage" );
+    compteurClone++;//Compter le nombre de clones
 });
+//Correction
+//$("#boutonDupliquer").click(function() {
+//     nbFlash++;
+//     $('#imgFlash').append('<img id="flashID'+nbFlash+'" class="flashClass" src="flash.jpg" width="200px"/>');
+//
+// });
 //Enlever une image
-$( "#boutonRalentir" ).click(function() {
-    $( "#flashID" ).remove();
+$("#boutonRalentir").click(function () {
+    if (compteurClone > 0) {//Pour garder l'image de Flash
+        $("#flashID").remove();
+        compteurClone--;
+    }
 });
+//Correction
+//$("#boutonRalentir").click(function() {
+//     $('#flashID'+nbFlash).remove();
+//     nbFlash--;
+// });
+
 //Téléportation
-let teleportation = "false";
+let teleportation = false;
 $( "#boutonTeleport" ).click(function() {
-    if(teleportation === "false") {
-        teleportation = "true";
+    if(!teleportation) {
+        teleportation = true;
         $('#flashID').hide();
         $('.centralCity').append('<img src="img/Flash1.png" id="flashTeleporte" width="200px"/>');
     }
     else{
-        teleportation = "false";
+        teleportation = false;
         $('#flashTeleporte').remove();//Utiliser remove après un append
         $('#flashID').show();
     }
 });
+//Correction
+//$("#boutonTeleport").click(function() {
+//     if($("#flashID").is(":visible")){
+//         $("#flashID").hide();
+//
+//         $("#flashIDBas").show();
+//     }
+//     else{
+//         $("#flashID").show();
+//
+//         $("#flashIDBas").hide();
+//     }
+// });
+
 //Vitesse suprême
-
-
 let monTimer;
 let vitesseSupreme = false;
 $( "#boutonVitesseSupreme" ).click(function() {
@@ -89,14 +153,25 @@ $( "#boutonNuke" ).click(function() {
     $(".boom").hide("slow");
 });
 //bouton perso1, changer l'opacité
+let camo = false;
 $( "#boutonPerso1" ).click(function() {
-    $("#flashID").fadeTo( "slow", 0.33 );
+    if(!camo) {
+        $("#flashID").fadeTo("slow", 0.33);
+        camo = true;
+    }
+    else{
+        $("#flashID").fadeTo("slow", 1);
+        camo = false;
+    }
 });
-//bouton perso2, changer la grandeur de l'image à chaque clic
+//bouton perso2, changer la grandeur de l'image à chaque clic de souris et retour normal avec un double-clic
 let grandeurImage = 200;
-$( "#boutonPerso2" ).click(function() {
+$("#boutonPerso2").click(function() {
      grandeurImage +=100;
     $("#flashID").attr( "width", grandeurImage );
 });
-
+$("#boutonPerso2").dblclick(function() {
+    grandeurImage = 200;
+    $("#flashID").attr( "width", 200);
+});
 
