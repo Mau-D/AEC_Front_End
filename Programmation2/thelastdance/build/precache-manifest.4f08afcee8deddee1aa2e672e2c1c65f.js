26bda1e5b72bc2db2ccfd09ed0a38f88204f26d3
Mau-D <harveymaud@gmail.com>
2020-04-26 14:49:35 -0400

    Premier projet React

---
+++ Programmation2/thelastdance/build/precache-manifest.4f08afcee8deddee1aa2e672e2c1c65f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4af0992bd8d718d8145cfc986c36e6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27a05b1ba998543da4e2",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "acba0160f3b65756036f",
+    "url": "./static/js/2.8f6e9f04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8f6e9f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a05b1ba998543da4e2",
+    "url": "./static/js/main.3fe420d6.chunk.js"
+  },
+  {
+    "revision": "f14872e1fc8ecf217aa0",
+    "url": "./static/js/runtime-main.f66a7021.js"
+  },
+  {
+    "revision": "b78b79b5f35d1aa3fe4ba2dda730a9c5",
+    "url": "./static/media/netflix.b78b79b5.png"
+  }
+]);
