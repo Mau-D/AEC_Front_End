6c4d2c63fe852d14a75c5d320cc5cc4c0d687061
Mau-D <harveymaud@gmail.com>
2020-05-07 19:29:47 -0400

    Components et props

--- Programmation2/becflix/build/service-worker.js
+++ Programmation2/becflix/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.514c9a20ddfca1f182f30825dd956ef5.js"
+  "./precache-manifest.70c2b20f6125c95dabce784c64a9b2a9.js"
 );
 
 self.addEventListener('message', (event) => {
