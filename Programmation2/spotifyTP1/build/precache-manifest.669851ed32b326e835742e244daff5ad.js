a1754a76ded96a7bdcfa85ac9d2bb0b76ba7f8bd
Mau-D <harveymaud@gmail.com>
2020-06-14 13:25:27 -0400

    TP! run build

---
+++ Programmation2/spotifyTP1/build/precache-manifest.669851ed32b326e835742e244daff5ad.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b552690f591b96898fc009f43a5b64e1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec625da87b1ae2963be5",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5e4f0efd9a4b089b5e1c",
+    "url": "./static/css/main.e7fc97ca.chunk.css"
+  },
+  {
+    "revision": "ec625da87b1ae2963be5",
+    "url": "./static/js/2.427baec4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.427baec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e4f0efd9a4b089b5e1c",
+    "url": "./static/js/main.a24c8270.chunk.js"
+  },
+  {
+    "revision": "b552f309936f792e8a60",
+    "url": "./static/js/runtime-main.5281adfe.js"
+  },
+  {
+    "revision": "9b95ac0e9ab01c6ec7d636ac60de4332",
+    "url": "./static/media/arcadeFire_thesuburbs.9b95ac0e.jpg"
+  },
+  {
+    "revision": "f6da7c9f309c03c2f8d4265b38db25f8",
+    "url": "./static/media/arrow.f6da7c9f.png"
+  },
+  {
+    "revision": "222c9e4b5fe9a48f3d088649358e0330",
+    "url": "./static/media/coeur.222c9e4b.png"
+  },
+  {
+    "revision": "44d3751e151a1958116063622dfd2ab3",
+    "url": "./static/media/equalizer.44d3751e.png"
+  },
+  {
+    "revision": "57f316ab8a0c7b05321e9dc799ded46a",
+    "url": "./static/media/jeanLeloup_ledome.57f316ab.jpg"
+  },
+  {
+    "revision": "0a75926e4c3f377fc81500f88e3d1bc2",
+    "url": "./static/media/nirvana_nevermind.0a75926e.jpg"
+  },
+  {
+    "revision": "f4f18833de75b59b285301750f49ff9d",
+    "url": "./static/media/nofx_punkindrublic.f4f18833.jpg"
+  },
+  {
+    "revision": "57f87459923db64bdc8864d174c07944",
+    "url": "./static/media/play.57f87459.png"
+  },
+  {
+    "revision": "f93acf1cf53b47b335b07237fb789d74",
+    "url": "./static/media/qc.f93acf1c.png"
+  },
+  {
+    "revision": "8d52481ab67deac4e4a756e50342135b",
+    "url": "./static/media/radiohead_okcomputer.8d52481a.jpg"
+  },
+  {
+    "revision": "50135548d2ce14ff966feb7d5b3a0e5b",
+    "url": "./static/media/theBlackKeys_brothers.50135548.jpg"
+  }
+]);
