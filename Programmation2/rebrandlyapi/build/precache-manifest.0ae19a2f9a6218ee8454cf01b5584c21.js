d75e88a2c822f328d97ddf08f9bbab686051d7db
Mau-D <harveymaud@gmail.com>
2020-06-25 10:16:49 -0400

    save gitignore

---
+++ Programmation2/rebrandlyapi/build/precache-manifest.0ae19a2f9a6218ee8454cf01b5584c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7814a543fcace54616f00bced2d5405",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a72c0156e0f41f16833a",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "488d705b4523a618287f",
+    "url": "./static/css/main.f2527a78.chunk.css"
+  },
+  {
+    "revision": "a72c0156e0f41f16833a",
+    "url": "./static/js/2.98d55f40.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.98d55f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488d705b4523a618287f",
+    "url": "./static/js/main.1519053b.chunk.js"
+  },
+  {
+    "revision": "90407819f4575a8f862f",
+    "url": "./static/js/runtime-main.2875373e.js"
+  }
+]);
