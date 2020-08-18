7fff7d5cfada8f768c4e32a75659bab892d65ba3
Mau-D <harveymaud@gmail.com>
2020-08-18 14:50:11 -0400

    build

---
+++ Programmation2/adventure_motorcycles/build/precache-manifest.e2641cfd6b96f30df0af26d28864c1ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea816f8442cbd1778ea4630513bfe6b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "85df6be4a79ba0c0cefc",
+    "url": "./static/css/2.e7c4c567.chunk.css"
+  },
+  {
+    "revision": "4f247d911e380366555f",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "85df6be4a79ba0c0cefc",
+    "url": "./static/js/2.37135301.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.37135301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f247d911e380366555f",
+    "url": "./static/js/main.2eea4bd9.chunk.js"
+  },
+  {
+    "revision": "234c2a93a15ca5cb69a4",
+    "url": "./static/js/runtime-main.5543b218.js"
+  },
+  {
+    "revision": "3e78af662ec5181763213394f199590d",
+    "url": "./static/media/404motorcycle.3e78af66.png"
+  }
+]);
