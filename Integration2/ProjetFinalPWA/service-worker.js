9c730980834faa3eef96a1a9bc920ea1789b04a1
Mau-D <harveymaud@gmail.com>
2020-08-26 17:37:50 -0400

    serviceworker completed

--- Integration2/ProjetFinalPWA/service-worker.js
+++ Integration2/ProjetFinalPWA/service-worker.js
@@ -23,6 +23,18 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
     console.log('[ServiceWorker] Activate');
 //Remove previous cached data from disk.
+    //Remove previous cached data from disk.
+    evt.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Removing old cache',
+                        key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
     self.clients.claim();
 });
 
@@ -30,4 +42,17 @@ self.addEventListener('activate', (evt) => {
 self.addEventListener('fetch', (evt) => {
     console.log('[ServiceWorker] Fetch', evt.request.url);
 //Add fetch event handler here.
+    if (evt.request.mode !== 'navigate') {
+// Not a page navigation, bail.
+        return;
+    }
+    evt.respondWith(
+        fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+                    .then((cache) => {
+                        return cache.match('/Cochenille/PointNClick/offline.html' );
+                    });
+            })
+    );
 });
