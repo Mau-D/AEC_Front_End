6e148481431c0026d3dd75401e8308dc31922d64
Mau-D <harveymaud@gmail.com>
2020-08-29 08:29:58 -0400

    version 3, ajout favicon

--- Integration2/ProjetFinalPWA/service-worker.js
+++ Integration2/ProjetFinalPWA/service-worker.js
@@ -2,10 +2,11 @@
 
 //Update cache names any time any of the cached files change.
 //Changer la version après chaque modification*******************
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v3';
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     'offline.html',
+    'img/icons8-deer.png',
 ];
 self.addEventListener('install', (evt) => {
     console.log('[ServiceWorker] Install');
