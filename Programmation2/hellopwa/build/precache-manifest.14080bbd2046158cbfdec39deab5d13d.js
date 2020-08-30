57afd8c62d4095cf5c9365d698ea4899978651f8
Mau-D <harveymaud@gmail.com>
2020-08-30 17:43:28 -0400

    bootstrap

---
+++ Programmation2/hellopwa/build/precache-manifest.14080bbd2046158cbfdec39deab5d13d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f730f54ee46e44e5f3977185777f24d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0939b6b1a6afed27e634",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "67e02f629d79537d20a3",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0939b6b1a6afed27e634",
+    "url": "./static/js/2.bc18c409.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.bc18c409.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67e02f629d79537d20a3",
+    "url": "./static/js/main.52a2e48e.chunk.js"
+  },
+  {
+    "revision": "596db6d2290197e71c87",
+    "url": "./static/js/runtime-main.cf705ba3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- Programmation2/hellopwa/src/App.js
+++ Programmation2/hellopwa/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { Button } from "react-bootstrap";
 function App() {
   return (
     <div className="App">
