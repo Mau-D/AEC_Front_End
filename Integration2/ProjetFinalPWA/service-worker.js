e7f9549676688ecf58f8505d7193b10f366e5148
Mau-D <harveymaud@gmail.com>
2020-09-01 08:41:13 -0400

    version 10

--- Integration2/ProjetFinalPWA/service-worker.js
+++ Integration2/ProjetFinalPWA/service-worker.js
@@ -2,7 +2,7 @@
 
 //Update cache names any time any of the cached files change.
 //Changer la version après chaque modification*******************
-const CACHE_NAME = 'static-cache-v9';
+const CACHE_NAME = 'static-cache-v10';
 //Add list of files to cache here.
 const FILES_TO_CACHE = [
     'offline.html',
