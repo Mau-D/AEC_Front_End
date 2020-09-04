3945e11206351b6fc8984e15bec2f333df3c1a1a
Mau-D <harveymaud@gmail.com>
2020-09-04 12:58:33 -0400

    synchoniser

---
+++ Programmation2/projetfinalreact/build/precache-manifest.445b8d125736b1aa8c71fc1de7072a4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c550ad4890c00df36b8e558177aa3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc2c9cc537b85c98b10c",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9ac8b823387797ca32f6",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dc2c9cc537b85c98b10c",
+    "url": "./static/js/2.1f6b1973.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1f6b1973.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ac8b823387797ca32f6",
+    "url": "./static/js/main.9c6910b6.chunk.js"
+  },
+  {
+    "revision": "3d1767f2ca6c6d50814a",
+    "url": "./static/js/runtime-main.fdb18bb5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
