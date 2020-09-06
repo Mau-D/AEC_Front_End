3ddccaa5e76168eba84fab3adcc624fe656193af
Mau-D <harveymaud@gmail.com>
2020-09-06 10:20:36 -0400

    https://mau-d.github.io/

--- Programmation2/adventure_motorcycles_hooks/src/index.js
+++ Programmation2/adventure_motorcycles_hooks/src/index.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router } from "react-router-dom";
 //"homepage": "https://mau-d.github.io/AEC_Front_End/Programmation2/adventure_motorcycles_hooks/build",
 ReactDOM.render(
-  <Router basename="https://mau-d.github.io/AEC_Front_End/Programmation2/adventure_motorcycles_hooks/build">
+  <Router basename="https://mau-d.github.io">
     <App />
   </Router>,
   document.getElementById("root")
