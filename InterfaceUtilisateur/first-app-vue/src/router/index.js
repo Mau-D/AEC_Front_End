0859674959c299ef3f30cbfcfd2e97cc2ab27175
Mau-D <harveymaud@gmail.com>
2021-02-28 13:30:36 -0500

    vue.config.js

--- InterfaceUtilisateur/first-app-vue/src/router/index.js
+++ InterfaceUtilisateur/first-app-vue/src/router/index.js
@@ -38,7 +38,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: "/AEC_Front_End/InterfaceUtilisateur/first-app-vue/dist",
+  base: process.env.BASE_URL,
   routes,
 });
 
---
+++ InterfaceUtilisateur/first-app-vue/vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  publicPath:
+    "/AEC_Front_End/tree/master/InterfaceUtilisateur/first-app-vue/dist/",
+};
